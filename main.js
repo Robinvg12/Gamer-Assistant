763ee5e658e02ee10a2252bf2f7dad016bc1cdb2
Robinvg12 <71080388+Robinvg12@users.noreply.github.com>
2020-12-14 12:23:53 +0100

    IT WORKS
    IT MF WORKS YEAH BOIIIIIIIIIIIIIIIIII

--- main.js
+++ main.js
@@ -180,13 +180,13 @@ client.on('message', message => {
     }
     else if (args[0] == "dice" || args[0] == "dobbelsteen" || args[0] == "roll") {
         args[0] = "1"
-        message.channel.send("args " + args);
+        //message.channel.send("args " + args);
         args_str = args.join(",");
-        message.channel.send("args join " + args_str);
+        //message.channel.send("args join " + args_str);
         args_str = args_str.replace(/[^0-9,]/g, '')
-        message.channel.send("args join replaced " + args_str);
+        //message.channel.send("args join replaced " + args_str);
         args = args_str.split(",");
-        message.channel.send("args split " + args)
+        //message.channel.send("args split " + args)
         message.channel.send("Rolling...")
         //message.channel.send("args.length " + args.length)
         //message.channel.send("args[0] " + args[0])
