e0c48caa37a898870891ab9e1d8aa8d8f72297cd
Robinvg12 <71080388+Robinvg12@users.noreply.github.com>
2021-01-08 13:51:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,13 +45,14 @@ client.on('message', message => {
     var str = message.content;
     var res = str.toLowerCase().replace(/[^A-Za-z0-9\s<>@:]/g, '');
 
+    console.log(str);
+
     if (res.split(/ +/)[0] == "mels") {
         message.channel.send("");
     }
 
     if (!res.startsWith(prefix) || message.author.bot) return;
 
-    console.log(res);
     //message.channel.send('mc: ' + message.content);
     //message.channel.send('res: ' + res);
 
