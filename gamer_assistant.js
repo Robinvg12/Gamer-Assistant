71c6664dfe40fe04f521e47523379b854889cc82
Robinvg12 <71080388+Robinvg12@users.noreply.github.com>
2021-01-09 16:23:30 +0100

    Update gamer_assistant.js

--- gamer_assistant.js
+++ gamer_assistant.js
@@ -61,7 +61,7 @@ client.on('message', message => {
     var str2 = message.content.toLowerCase();
     var res = str.toLowerCase().replace(/[^A-Za-z0-9\s<>@:]/g, '');
 
-    //console.log(str);
+    console.log(str);
 
     if (res.split(/ +/)[0] == "mels") {
         message.channel.send("<@!457062141078536194>").then(msg => msg.delete({ timeout: 10}));;
